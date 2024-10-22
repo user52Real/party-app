2d3d08bbdf5c72a1fea147f6f7973eb653109ac2
user52 <145881992+user52Real@users.noreply.github.com>
2024-10-22 13:57:32 +0000

    vercel fixes second attempt

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils"
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>(
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
